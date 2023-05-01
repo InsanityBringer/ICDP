adc119ac08ea5438031a1e454b0680dcf4ebf7b5
InsanityBringer <1504999+InsanityBringer@users.noreply.github.com>
2023-05-01 12:37:39 -0500

    backport fullscreen fix from chocolate

--- platform/sdl/gr_sdl.cpp
+++ platform/sdl/gr_sdl.cpp
@@ -93,8 +93,6 @@ int plat_create_window()
 	//SDL is good, create a game window
 	gameWindow = SDL_CreateWindow(titleMsg, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WindowWidth, WindowHeight, flags);
 	//int result = SDL_CreateWindowAndRenderer(WindowWidth, WindowHeight, flags, &gameWindow, &renderer);
-	if (Fullscreen)
-		SDL_GetWindowSize(gameWindow, &CurWindowWidth, &CurWindowHeight);
 
 	if (!gameWindow)
 	{
@@ -114,6 +112,9 @@ int plat_create_window()
 	}
 
 	SDL_ShowWindow(gameWindow);
+
+	if (Fullscreen)
+		SDL_GetWindowSize(gameWindow, &CurWindowWidth, &CurWindowHeight);
 
 	return 0;
 }
